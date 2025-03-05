cb08ef01eff9c00cb67359c0c68d368153baee85
MahdyarMaster <m.mirshaky@gmail.com>
2025-03-05 11:15:43 +0330

    Simple Launcher and nethandler

--- Handlers/nethandler.cs
+++ Handlers/nethandler.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System;
+using System.Net;
+using System.Net.Sockets;
 
-namespace MCServer_CSharp_Edition.Handlers
-{
-    public class nethandler
-    {
-        
+class NetHandler {
+    public static void StartServer() {
+        TcpListener server = new TcpListener(IPAddress.Any, 25565);
+        server.Start();
+        Console.WriteLine("Server started on port 25565");
+
+        while (true) {
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("New connection!");
+            // Handle client in another method/thread
+        }
     }
 }
