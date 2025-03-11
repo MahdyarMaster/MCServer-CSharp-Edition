eabb18ed6c2e6a191bf975bdd2302c5c30a98bd4
MahdyarMaster <m.mirshaky@gmail.com>
2025-03-11 19:29:26 +0330

    Just a simple update!

--- obj/Debug/net9.0/MCServer-CSharp-Edition.AssemblyInfo.cs
+++ obj/Debug/net9.0/MCServer-CSharp-Edition.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MCServer-CSharp-Edition")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb08ef01eff9c00cb67359c0c68d368153baee85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a0c83c914af63123044446995741e7e097c4019")]
 [assembly: System.Reflection.AssemblyProductAttribute("MCServer-CSharp-Edition")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MCServer-CSharp-Edition")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
